30667fe877c327bbca77d7da9f0b5d9ad09a9c2b
Norway174 <norway174@terrancom.net>
2017-08-27 06:22:59 +0200

    Added a new command: translate
    This translate commands will automatically detect the language, and
    translate it for you.
    
    It will only translate to English. For now.

---
+++ commands/translate.js
@@ -0,0 +1,158 @@
+const Discord = require("discord.js");
+const translate = require('google-translate-api');
+//const langs = require("../util/langs.json")
+
+exports.run = (client, message, args) => {
+	
+	translate(args.join(" "), {to: 'en'}).then(res => {
+		
+		let lang = res.from.language.iso
+		
+		//Here, we build the emblem for the online server.
+				const embed = new Discord.RichEmbed()
+					//.setTitle("Translate")
+					.setColor(0x4D90FE)
+					//.setDescription( res.text )
+					.setFooter( "Translated for " + message.author.username + "." )
+					.addField("Detected: " + langs[lang], args.join(" "), true)
+					.addBlankField(true)
+					.addField("English", res.text, true)
+					//.setThumbnail("http://i.imgur.com/2JUhMfW.png")
+					//.setTimestamp()
+				
+				//And then edit the first message we sent. We don't want duplicate messages in our chat.
+				message.channel.send({embed});
+		
+		//console.log(res.text);
+		//=> I speak English
+		//console.log(langs[lang]);
+		//=> nl
+	}).catch(err => {
+		console.error(err);
+	});
+	
+	//message.delete(5);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['t'],
+  permLevel: 1
+};
+
+exports.help = {
+  name: 'translate',
+  description: 'Translates the given text to English.',
+  usage: 'translate <any text>'
+};
+
+
+const langs = {
+    'auto': 'Automatic',
+    'af': 'Afrikaans',
+    'sq': 'Albanian',
+    'am': 'Amharic',
+    'ar': 'Arabic',
+    'hy': 'Armenian',
+    'az': 'Azerbaijani',
+    'eu': 'Basque',
+    'be': 'Belarusian',
+    'bn': 'Bengali',
+    'bs': 'Bosnian',
+    'bg': 'Bulgarian',
+    'ca': 'Catalan',
+    'ceb': 'Cebuano',
+    'ny': 'Chichewa',
+    'zh-cn': 'Chinese Simplified',
+    'zh-tw': 'Chinese Traditional',
+    'co': 'Corsican',
+    'hr': 'Croatian',
+    'cs': 'Czech',
+    'da': 'Danish',
+    'nl': 'Dutch',
+    'en': 'English',
+    'eo': 'Esperanto',
+    'et': 'Estonian',
+    'tl': 'Filipino',
+    'fi': 'Finnish',
+    'fr': 'French',
+    'fy': 'Frisian',
+    'gl': 'Galician',
+    'ka': 'Georgian',
+    'de': 'German',
+    'el': 'Greek',
+    'gu': 'Gujarati',
+    'ht': 'Haitian Creole',
+    'ha': 'Hausa',
+    'haw': 'Hawaiian',
+    'iw': 'Hebrew',
+    'hi': 'Hindi',
+    'hmn': 'Hmong',
+    'hu': 'Hungarian',
+    'is': 'Icelandic',
+    'ig': 'Igbo',
+    'id': 'Indonesian',
+    'ga': 'Irish',
+    'it': 'Italian',
+    'ja': 'Japanese',
+    'jw': 'Javanese',
+    'kn': 'Kannada',
+    'kk': 'Kazakh',
+    'km': 'Khmer',
+    'ko': 'Korean',
+    'ku': 'Kurdish (Kurmanji)',
+    'ky': 'Kyrgyz',
+    'lo': 'Lao',
+    'la': 'Latin',
+    'lv': 'Latvian',
+    'lt': 'Lithuanian',
+    'lb': 'Luxembourgish',
+    'mk': 'Macedonian',
+    'mg': 'Malagasy',
+    'ms': 'Malay',
+    'ml': 'Malayalam',
+    'mt': 'Maltese',
+    'mi': 'Maori',
+    'mr': 'Marathi',
+    'mn': 'Mongolian',
+    'my': 'Myanmar (Burmese)',
+    'ne': 'Nepali',
+    'no': 'Norwegian',
+    'ps': 'Pashto',
+    'fa': 'Persian',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'ma': 'Punjabi',
+    'ro': 'Romanian',
+    'ru': 'Russian',
+    'sm': 'Samoan',
+    'gd': 'Scots Gaelic',
+    'sr': 'Serbian',
+    'st': 'Sesotho',
+    'sn': 'Shona',
+    'sd': 'Sindhi',
+    'si': 'Sinhala',
+    'sk': 'Slovak',
+    'sl': 'Slovenian',
+    'so': 'Somali',
+    'es': 'Spanish',
+    'su': 'Sundanese',
+    'sw': 'Swahili',
+    'sv': 'Swedish',
+    'tg': 'Tajik',
+    'ta': 'Tamil',
+    'te': 'Telugu',
+    'th': 'Thai',
+    'tr': 'Turkish',
+    'uk': 'Ukrainian',
+    'ur': 'Urdu',
+    'uz': 'Uzbek',
+    'vi': 'Vietnamese',
+    'cy': 'Welsh',
+    'xh': 'Xhosa',
+    'yi': 'Yiddish',
+    'yo': 'Yoruba',
+    'zu': 'Zulu'
+};

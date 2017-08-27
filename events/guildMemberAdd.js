a9e6a06c0419861bce2e15bce45859b0e725eb34
Norway174 <norway174@terrancom.net>
2017-08-27 19:41:01 +0200

    Improved member welcome message slightly
    Now the welcome message should look even better, and eaiser to
    understand.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,6 +8,6 @@ module.exports = member => {
 	const embed = new Discord.RichEmbed()
 		//.setTitle("Status updated")
 		.setColor(0x90FF00)
-		.setDescription(`Hi there, ${member.user.username}!\nWelcome to ${guild.name}, feel free to introduce yourself. Or ask if you have any questions.\nYou may also use ${settings.prefix}help to see what I can do for you.\n\nEnjoy your stay!`)
+		.setDescription(`**Hi there, ${member.user.username}!**\nWelcome to ${guild.name}! Feel free to introduce yourself; don't be afraid to ask any questions!\nYou may also use ` + "`" + `${settings.prefix}help` + "`" + ` to see what I can do for you.\n\nEnjoy your stay!`)
 	guild.defaultChannel.send({embed});
 };

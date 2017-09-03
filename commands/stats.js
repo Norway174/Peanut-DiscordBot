cbf68688975171d187ec1af7cf71ce8ea804131a
Norway174 <norway174@terrancom.net>
2017-09-03 12:55:08 +0200

    Added a stats command & widget
    Can now display some semi-real-time stats about the bot. As either a
    dynamically updating widget. Or via a command.

---
+++ commands/stats.js
@@ -0,0 +1,33 @@
+const { version } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+
+exports.run = function(client, message, args){ // eslint-disable-line no-unused-vars
+	this.client = client;
+	
+    // const guilds = (await client.shard.broadcastEval('this.guilds.size'));//.reduce((a, b) => a + b, 0);
+    // console.log(guilds);
+    const duration = moment.duration(this.client.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
+    message.channel.send(`= STATISTICS =
+  • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+  • Uptime     :: ${duration}
+  • Users      :: ${this.client.users.size.toLocaleString()}
+  • Servers    :: ${this.client.guilds.size.toLocaleString()}
+  • Channels   :: ${this.client.channels.size.toLocaleString()}
+  • Discord.js :: v${version}
+  • Node       :: ${process.version}
+  • Source     :: https://github.com/Norway174/Peanut-DiscordBot`, {code: 'asciidoc'});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['system', 'usage'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'stats',
+  description: 'This command will display system stats such as memory used, uptime and version.',
+  usage: 'stats'
+};
---
+++ widgets/stats.js
@@ -0,0 +1,55 @@
+const { version } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+const log = message => {
+	console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+exports.run = function(client, widget, data){
+	log(`WIDGET PROCESSED: ${widget.name}`);
+	//message.channel.send("Current time: '" + Date.now() + "'");
+	
+	/*
+	const widgetSettings = {
+		serverID: widget.serverID,
+		channelID: widget.channelID,
+		messageID: widget.messageID,
+		name: widget.name,
+		type: widget.type,
+		interval: widget.interval,
+		intervalCount: 0,
+		data: widget.data
+	}
+	
+	//console.log(widgetSettings)
+	client.widgets.set(widget.name, widgetSettings);
+	*/
+	
+	client.guilds.get(widget.serverID).channels.get(widget.channelID).fetchMessage(widget.messageID).then(m => {
+			this.client = client;
+	
+    // const guilds = (await client.shard.broadcastEval('this.guilds.size'));//.reduce((a, b) => a + b, 0);
+    // console.log(guilds);
+    const duration = moment.duration(this.client.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
+    m.edit(`= STATISTICS =
+  • Mem Usage   :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+  • Uptime      :: ${duration}
+  • Users       :: ${this.client.users.size.toLocaleString()}
+  • Servers     :: ${this.client.guilds.size.toLocaleString()}
+  • Channels    :: ${this.client.channels.size.toLocaleString()}
+  • Discord.js  :: v${version}
+  • Node        :: ${process.version}
+  • Source      :: https://github.com/Norway174/Peanut-DiscordBot
+
+= WIDGET INFORMATION =
+  • Widget Name :: ${widget.name}
+  • Updates     :: Every ${widget.interval} minutes`, {code: 'asciidoc'});
+	});
+	
+};
+
+exports.help = {
+  name: 'stats',
+  description: 'Used to display the bot stats.',
+  usage: 'NA'
+};

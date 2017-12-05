1ffb6da74c53a2e14d801812e06271ffb80db106
Norway174 <norway174@terrancom.net>
2017-12-05 20:17:05 +0100

    Changed deprecated function in reload.js
    .sendMessage changed to .send in reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -6,9 +6,9 @@ exports.run = (client, message, args) => {
 		command = client.aliases.get(args[0]);
 	}
 	if (!command) {
-		return message.channel.sendMessage(`I cannot find the command: ${args[0]}`);
+		return message.channel.send(`I cannot find the command: ${args[0]}`);
 	} else {
-		message.channel.sendMessage(`Reloading: ${command}`)
+		message.channel.send(`Reloading: ${command}`)
 			.then(m => {
 				client.reload(command)
 					.then(() => {

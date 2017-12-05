bf692e67f82cf5e3e21652b390e7f649c2018717
Norway174 <norway174@terrancom.net>
2017-12-05 20:23:42 +0100

    Added new command: joke.js
    Uses node-fetch.
    
    Simple fetched a joke from "https://icanhazdadjoke.com/" in plain text,
    and displays it back as a message.
    
    Might add some formating later, but it works for now. And it's quite fun
    to use.

---
+++ commands/joke.js
@@ -0,0 +1,28 @@
+var fetch = require("node-fetch");
+
+exports.run = function(client, message){
+
+	fetch("https://icanhazdadjoke.com/", { headers: {"Accept":"text/plain", "User-Agent":"Peanut (https://github.com/Norway174/Peanut-DiscordBot)"}})
+		.then(res => {
+			return res.text();
+		}).then(function(body) {
+			client.log("JOKE: " + body);
+			message.channel.send(body);
+		});
+
+
+	
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	permLevel: 0
+};
+
+exports.help = {
+	name: "joke",
+	description: "The best dad jokes! Courtesy of https://icanhazdadjoke.com/",
+	usage: "joke"
+};

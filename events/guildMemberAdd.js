e50cf0fc277ac897d152ed6461334cd0d44f91b1
Norway174 <norway174@terrancom.net>
2017-12-21 08:18:38 +0100

    Small change to join/leave messages
    There's a Discord bug where if you tag a member, that tag name may
    display just the ID in a embed. Especially if the user has left the
    channel.
    
    And on phones, it won't display the name at all. Only the ID.
    
    With this change, it will display the name with a tag as plain text in a
    prenthesis next to the tagged name. So if the tag disapears, you can
    still see who it was.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -11,7 +11,7 @@ module.exports = member => {
 	const embed = new Discord.RichEmbed()
 		//.setTitle("Status updated")
 		.setColor(0x90FF00)
-		.setDescription(`**Hi there, <@${member.user.id}>!**\nWelcome to ${guild.name}! Feel free to introduce yourself; don't be afraid to ask any questions!\nYou may also use ` + "`" + `${settings.prefix}help` + "`" + " to see what I can do for you.\n\nEnjoy your stay!");
+		.setDescription(`**Hi there, <@${member.user.id}>!** (${member.user.tag})\nWelcome to ${guild.name}! Feel free to introduce yourself; don't be afraid to ask any questions!\nYou may also use ` + "`" + `${settings.prefix}help` + "`" + " to see what I can do for you.\n\nEnjoy your stay!");
 	channel.send({embed});
 	
 	client.log(`${member.user.tag} has joined ${guild.name}.`);

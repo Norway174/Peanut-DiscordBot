5f12ec34bf9544404bd7ac1380fa0b0c001c092f
Norway174 <norway174@terrancom.net>
2018-02-06 19:01:16 +0100

    Cleaned up the widgetLoader
    Removed all the useless debugging info that was commented out.
    
    Made the file significantly smaller. And hopefully, much eaiser to read,
    and less likely to cause any problems in the future.

--- util/widgetLoader.js
+++ util/widgetLoader.js
@@ -4,17 +4,12 @@ const moment = require("moment");
 
 module.exports = client => {
 	client.on("ready", () => {
-		
-		//Load the widgets from the PCollection.
-		//client.widgets
-		
+
 		function processWidget() {
-			//client.log(`Procesing ${client.widgets.size} widgets.`);
 
 			client.widgets.forEach(widget => {
-				//widget = JSON.parse(widget);
 				/*
-				const widgetSettings = {
+				widget = {
 					serverID: widget.serverID,
 					channelID: widget.channelID,
 					messageID: widget.messageID,
@@ -24,39 +19,27 @@ module.exports = client => {
 					intervalCount: 0,
 					data: widget.data
 				}
-				
-				//client.log(widgetSettings)
-				client.widgets.set(widget.name, widgetSettings);
 				*/
-				
-				
-				//client.log(client.widgets.constructor.name, typeof client.widgets);
-				
-				//client.log(`WIDGET ${widget}`);
-				
+			
 				let intervalCount = widget.intervalCount + 1;
-				
-				//client.log(`WIDGET ${widget.name}, ${widget.type}, ${widget.intervalCount} / ${widget.interval}`);
-				
-				//client.log(`WIDGET Interval: ${widget.interval} Count: ${widget.intervalCount}`);
+
 				if(intervalCount >= widget.interval){
-					//client.log(`WIDGET Count: ${widget.intervalCount} Reseting to zero`);
 					intervalCount = 0;
-					
-					
+
+					/*if(client.guilds.get(widget.serverID).channels.get(widget.channelID) != isNullOrUndefined){
+
+					}*/
+										
 					if (client.widgetsType.has(widget.type)){
 						client.reloadWidget(widget.type);
-					
-						//client.log(`WIDGET TYPE Reloaded: ${widget.type}`);
-					
-					
+
 						let command = widget.type;
 						let cmd = client.widgetsType.get(command);
 
 						if (cmd) {
-						//client.log(`WIDGET PROCESSED: ${widget.name}`);
+
 							cmd.run(client, widget);
-						//log(`[USER: ${message.author.tag}] [${sourceLoc}] [COMMAND: ${msg}] [RESULT: Success.]`)
+
 						}
 					
 					
@@ -71,12 +54,12 @@ module.exports = client => {
 		}
 		
 		
+		// 60000 miliseconds = 1 minute
 		var interval = 60 * 1000;
 		let now = new Date();
 		let delay = interval - now % interval;
 
 		
-		// 60000 miliseconds = 1 minute
 		setTimeout(() => {
 			processWidget();
 			setInterval(processWidget, interval);

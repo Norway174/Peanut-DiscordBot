fe2eb18698eea573ebb94e0432c936574a2baf61
Norway174 <norway174@terrancom.net>
2018-02-11 18:21:27 +0100

    Small change to the way perms are calculated
    Now, it will check if the user has the "ADMINISTRATOR" privilege to
    allow guild affecting commands. Such as the guild spesific settings.

--- bot.js
+++ bot.js
@@ -72,11 +72,9 @@ client.elevation = message => {
 	}
 
 
-	let mod_role = message.guild.roles.find("name", settings.modrolename);
-	if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
-	let admin_role = message.guild.roles.find("name", settings.adminrolename);
-	if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 3;
-	if (message.author.id === settings.ownerid) permlvl = 4;
+	if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+
+	if (message.author.id === client.botSettings.ownerid) permlvl = 4;
 	return permlvl;
 };
 

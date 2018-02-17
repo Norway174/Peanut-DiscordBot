d489fd15bea816ffb54a9ef90e5118ea363c7d6a
Norway174 <norway174@terrancom.net>
2018-02-18 00:36:08 +0100

    Added Argsplit and updated the message handler
    Added Argsplit as a dependency from npm to the message handler.
    
    Now it will properly split up the message args for commands. And it will
    even take into considerations messages containing double quotes ( " " ),
    in order to use whole sentences as one argument.

--- events/message.js
+++ events/message.js
@@ -1,51 +1,75 @@
 //const = require("../json");
 const moment = require("moment");
+const argsplit = require("argsplit");
 
 module.exports = message => {
+	// Ignore other bots!
 	if (message.author.bot) return;
 
+	// Definitions
 	let client = message.client;
 	let guild = message.guild;
 	let settings = client.settings.get(guild.id);
 	let prefix = settings.prefix;
 	
+	// Check if message is a command
 	let iscmd = false;
 	if (message.content.startsWith(prefix)) iscmd = true;
 	if (message.content.startsWith(client.user)) iscmd = true;
 	if (message.channel.type == "dm") iscmd = true;
 	
+	// Stop if not a command
 	if (!iscmd) return;
 	
+	// Replace bot mention with the prefix & add prefix if there is none in the DM channel
 	let msg = message.content;
-	if (message.content.startsWith(client.user)) msg = msg.replace(client.user + " ", prefix);
-	if (message.channel.type == "dm" && !msg.startsWith(prefix)) msg = prefix + msg;
+	if (message.content.startsWith(client.user)) msg = msg.replace(client.user + " ", "");
+	//if (message.channel.type == "dm" && !msg.startsWith(prefix)) msg = msg;
+	if (message.content.startsWith(prefix)) msg = msg.replace(prefix, "");
 	
-	let command = msg.split(" ")[0].slice(prefix.length).toLowerCase();
-	let params = msg.split(" ").slice(1);
+	// Commander args handler
+	let command = msg.split(" ")[0];
+	msg = msg.replace(command, "");
+
+	let params = argsplit(msg);
+
+	// Perms handler, elevate the perm if necessary
 	let perms = client.elevation(message);
+
+	// Define a null command.
 	let cmd;
 	
+	// Source formating for the server log
 	let sourceLoc;
 	if (message.channel.type == "dm" ) sourceLoc = `DM: ${message.channel.recipient.tag}`;
 	if (message.channel.type == "group" ) sourceLoc = `GroupDM: ${message.channel.recipients}`;
 	if (message.channel.type == "text" ) sourceLoc = `SERVER: ${message.guild.name} / ${message.channel.name}`;
 	if (message.channel.type == "voice" ) sourceLoc = "Voice: This shoulden't happen?";
 	
+	// Command handler
+	command = command.toLowerCase();
 	if (client.commands.has(command)) {
+		// Get command from name
 		cmd = client.commands.get(command);
 	} else if (client.aliases.has(command)) {
+		// Get command from alias
 		cmd = client.commands.get(client.aliases.get(command));
 	} else {
-		client.log(`[USER: ${message.author.tag}] [${sourceLoc}] [COMMAND: ${msg}] [RESULT: Not found.]`);
+		// If no command is found
+		client.log(`[USER: ${message.author.tag}] [${sourceLoc}] [COMMAND: ${command}] [RESULT: Not found.]`);
 		message.channel.send(`No command found. Type '${prefix}help'`, {code:"xl"});
 	}
+	// If the command is found
 	if (cmd) {
+		// Check the permission level for the command
 		if (perms < cmd.conf.permLevel){
-			client.log(`[USER: ${message.author.tag}] [${sourceLoc}] [COMMAND: ${msg}] [RESULT: No access.]`);
+			// No permission!
+			client.log(`[USER: ${message.author.tag}] [${sourceLoc}] [COMMAND: ${command}] [RESULT: No access.]`);
 			message.channel.send("Access denied!", {code:"xl"});
 			return;
 		}
-		client.log(`[USER: ${message.author.tag}] [${sourceLoc}] [COMMAND: ${msg}] [RESULT: Success.]`);
+		// Log & run the command!
+		client.log(`[USER: ${message.author.tag}] [${sourceLoc}] [COMMAND: ${command}] [RESULT: Success.]`);
 		cmd.run(client, message, params, perms);
 	}
 

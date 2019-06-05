217c716468412993be1cab79ae931600b8e60939
Norway174 <norway174@terrancom.net>
2019-06-05 04:02:19 +0200

    Role-reactor now functional!
    YAY!

--- commands/role-reactor.js
+++ commands/role-reactor.js
@@ -1,22 +1,95 @@
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 	var msg = message.content;
 	var lines = msg.split("\n");
 	
 	// REGEX to find Unicode Emoji OR Discord Custom Emoji
-	const regex = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{2934}-\u{1f18e}]|<.[a-zA-Z0-9]*.[0-9]*>/gu;
+	const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c\ude32-\ude3a]|[\ud83c\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])|<:[a-zA-Z0-9]*:[0-9]*>/gm;
 
-	console.log(lines);
+	const roleRegex = /<@&[0-9]*>/;
 
-	lines.forEach(line => {
-		var match = line.match(regex);
-		match = match ? true : false;
-		 
+	const EmojiRegex = /<:[a-zA-z0-9]*:([0-9]*)>/;
+
+	//console.log(lines);
+
+	var ReactionsRole = [];
+
+	await lines.forEach(async (line) => {
+		var emoji = line.match(regex);
+		//match = match ? true : false;
+
+		var role = line.match(roleRegex);
+		
+		
+
+		if(emoji && role){
+			role = role.join("").replace("<@&", "").replace(">", "");
+			role = message.guild.roles.get(role);
+			emoji = emoji.join("").replace(EmojiRegex, `$1`);
+
+			//console.log("Line: " + line + " | Emoji: " + emoji + " | Role: " + role);
+
+			
+
+			ReactionsRole.push({emoji: emoji, role: role.id, id: message.id})
+
+		}
+
+
 	});
+
+	//console.log(ReactionsRole);
+
+	if(ReactionsRole){
+
+		client.reactionsRole.set(message.id, ReactionsRole);
+
+		ReactionsRole.forEach(async (ReactRole) => {
+			
+			message.react(ReactRole.emoji)
+				.catch(err => {
+					console.log("Error adding role: " + err);
+				});
+
+		});
+
+
+	}
 
 };
 
-exports.reactions = (client, packet) => {
+exports.reactions = async (client, packet, reactionRole) => {
 
+	var guild = client.guilds.get(packet.d.guild_id);
+
+	if(!guild.me.hasPermission("MANAGE_ROLES")){
+		client.logger.warn("RoleReactions Error: Missing permissions!")
+		return;
+	}
+
+	//console.log(ReactionRole);
+
+	var reactEmoji = reactionRole.find(re => re.emoji == packet.d.emoji.id || re.emoji == packet.d.emoji.name);
+	if(!reactEmoji) return;
+	var reactRole = guild.roles.get(reactEmoji.role);
+
+	console.log(reactRole);
+
+	var member = guild.members.get(packet.d.user_id)
+
+	console.log(member);
+
+	if (packet.t === 'MESSAGE_REACTION_ADD') {
+		//client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
+
+		member.addRole(reactRole).catch(console.error);
+
+	} else
+	if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+		//if(!reaction) reaction = packet.d.emoji;
+		//client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
+
+		member.removeRole(reactRole).catch(console.error);
+	}
 
 
 };
--- util/reactionsRoleLoader.js
+++ util/reactionsRoleLoader.js
@@ -11,54 +11,41 @@ client.on('messageReactionRemove', (reaction, user) => {
 */
 module.exports = client => {
 	client.on('raw', packet => {
-		// We don't want this to run on unrelated packets. We also need this here, because the other events won't fire unless the message is cached. Which it may not always be.
-		if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+		
+		if (['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) {
+			messageReact(client, packet);
+		} else
+		if (['MESSAGE_DELETE'].includes(packet.t)){
+			client.reactionsRole.forEach( (reactRole, key ) => {
+				if(!reactRole) return;
+				client.channels.get(packet.d.channel_id).fetchMessage(key).catch(err => {
+					client.reactionsRole.delete(key);
+					client.logger.log("ReactionsRole enabled message has been deleted: " + key);
+				});
+			});
+			
+		}
 
-		messageReact(client, packet);
-
-		// No point in continueing at the moment.
-
-		/*
-		// Grab the channel to check the message from
-		const channel = client.channels.get(packet.d.channel_id);
-		// There's no need to emit if the message is cached, because the event will fire anyway for that
-		if (channel.messages.has(packet.d.message_id)) return;
-		// Since we have confirmed the message is not cached, let's fetch it
-		channel.fetchMessage(packet.d.message_id).then(message => {
-			// Emojis can have identifiers of name:id format, so we have to account for that case as well
-			const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-			// This gives us the reaction we need to emit the event properly, in top of the message object
-			var reaction = message.reactions.get(emoji);
-			// Adds the currently reacting user to the reaction's users collection.
-			if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
-			// Check which type of event it is before emitting
-			if (packet.t === 'MESSAGE_REACTION_ADD') {
-				client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
-			}
-			if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-				if(!reaction) reaction = packet.d.emoji;
-				client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
-			}
-		});
-		*/
 	});
-
 };
 
 function messageReact(client, packet){
 
-	let reactionsRole = client.reactionsRole;
+	let reactionsRole = client.reactionsRole.get(packet.d.message_id);
 
-	reactionsRole.forEach(reactionsRole => {
-		/*if(widget.type == "giverole" && widget.messageID == packet.d.message_id){
+	//console.log("Reaction registered!");
 
-			var cmdName = "giverole"
-			client.reload(cmdName)
-				.then(() => {
-					cmd = client.commands.get(cmdName);
-					cmd.run(client, widget, widget.data, packet);
-				});
-		}*/
-	});
+
+	//var reactionRole = reactionsRole.find(packet.d.message_id)
+
+	if(!reactionsRole) return;
+
+	var cmdName = "role-reactor"
+	client.reload(cmdName)
+		.then(() => {
+			cmd = client.commands.get(cmdName);
+			cmd.reactions(client, packet, reactionsRole);
+		});
+		
 
 };

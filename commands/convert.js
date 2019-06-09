5b4a498c1ad927098d072aeb34b01176d0b41d73
Norway174 <norway174@terrancom.net>
2019-06-09 18:36:36 +0200

    Lot's of changes and fixes to the convert command

--- commands/convert.js
+++ commands/convert.js
@@ -7,15 +7,34 @@ exports.run = async function(client, message, args){
 	var getRatesReturn = await getRates(client, message);
 	var rates = getRatesReturn[0];
 	var msg = getRatesReturn[1];
-	var keys = Object.keys(rates)
+	var keys = Object.keys(rates);
 
-	//console.log("Fetched Rates.");
+	function checkInput(input, words) {
+		return words.some(word => input.toLowerCase().includes(word.toLowerCase()));
+	}
+	if(checkInput(message.content, ["codes", "code"])){
+		const embedInvalid = new Discord.RichEmbed()
+			.setDescription(`All avalible currency codes:\n\`\`\`${keys.map(key => `${key}	`).join("")}\`\`\``)
+			.setColor(0x78FF00)
+			.setFooter("Codes supplied by Fixer.io")
+			.setTimestamp();
+
+		if(msg){
+			msg.edit(embedInvalid);
+		} else {
+			message.channel.send(embedInvalid);
+		}
+		return;
+	} else
+	if(checkInput(message.content, ["rates", "rate"])){
+		ShowRates(client, message, msg, keys, rates);
+		return;
+	}
 
 	var amount = args[0];
-	var from = args[1].toUpperCase();
-	var to = args[2].toUpperCase();
+	var from = args[1];
+	var to = args[2];
 
-	var isValid = true
 
 	function getSymbol(symbol) {
 		var symbols = {
@@ -31,23 +50,43 @@ exports.run = async function(client, message, args){
 	from = getSymbol(from);
 	to = getSymbol(to);
 	
+	var isValid = true;
+	var reason = [];
 	// Check if the first arg is a number.
 	if(isNaN(amount)){
-		isValid = false
+		isValid = false;
+		reason.push(amount? `\`${amount}\` is not a valid number.` : `Missing \`<amount>\` to convert.`);
 	}
 	// Check if the first rate exists.
-	if(!keys.includes(from)){
-		isValid = false
+	if(!from || !keys.includes(from.toUpperCase())){
+		isValid = false;
+		reason.push(from? `\`${from}\` is not a valid currency code.` : `Missing \`<from>\` currency code.`);
 	}
 	// Check if the second rate exists.
-	if(!keys.includes(to)){
-		isValid = false
+	if(!to || !keys.includes(to.toUpperCase())){
+		isValid = false;
+		reason.push(to? `\`${to}\` is not a valid currency code.` : `Missing \`<to>\` currency code.`);
 	}
 
 	if(!isValid) {
-		message.channel.send(`Invalid format. Please use: \n\`\`\`${client.getSettings(message.guild).prefix}${this.help.usage}\`\`\``);
+		var customKeys = ['USD', 'CAD', 'EUR', 'NOK', 'GBP', 'BTC'];
+		const embedInvalid = new Discord.RichEmbed()
+			.setDescription(`${reason.join("\n")}\nPlease use: \n\`\`\`${client.getSettings(message.guild).prefix}${this.help.usage}\`\`\`\nCommon currency codes:\n\`\`\`${customKeys.map(key => `${key}	`).join("")}\`\`\``)
+			.setColor(0xFA3C3C)
+			.setFooter("Ooops! Something went wrong...")
+			.setTimestamp();
+
+		if(msg){
+			msg.edit(embedInvalid);
+		} else {
+			message.channel.send(embedInvalid);
+		}
+		//message.channel.send(`Invalid format. Please use: \n\`\`\`${client.getSettings(message.guild).prefix}${this.help.usage}\`\`\``);
 		return;
 	}
+
+	from = from.toUpperCase();
+	to = to.toUpperCase();
 
 	var fromAmount = amount / rates[from];
 	var toAmount = fromAmount * rates[to];
@@ -56,10 +95,10 @@ exports.run = async function(client, message, args){
 	toAmount = new Intl.NumberFormat({ style: 'currency', currency: to }).format(Math.round(toAmount * 100) / 100);
 
 	const embed = new Discord.RichEmbed()
-	.setTitle(`${amount} ${from} = ${toAmount} ${to}`) // ${Math.round(toAmount * 100) / 100}
-	.setColor(0x78FF00)
-	.setFooter("Converted used Fixer.io")
-	.setTimestamp();
+		.setTitle(`${amount} ${from} = ${toAmount} ${to}`) // ${Math.round(toAmount * 100) / 100}
+		.setColor(0x78FF00)
+		.setFooter("Converted using Fixer.io")
+		.setTimestamp();
 
 	if(msg){
 		msg.edit(embed);
@@ -80,7 +119,7 @@ exports.conf = {
 exports.help = {
 	name: "convert",
 	description: "Convert from one currency to another.",
-	usage: "convert <amount> <from> <to>"
+	usage: "convert <amount> <from> <to>\n{prefix}convert codes OR rates"
 };
 
 async function getRates(client, message){
@@ -145,3 +184,82 @@ async function getRates(client, message){
 	return [exchangeRates.rates, msg];
 
 };
+
+async function ShowRates(client, message, msg, keys, rates){
+
+	const embedRates = new Discord.RichEmbed()
+		.setDescription(`All avalible currency rates:`)
+		.setColor(0x78FF00)
+		.setFooter("Rates supplied by Fixer.io")
+		.setTimestamp();
+
+	var ratesBuilder = [];
+	for (let index = 0; index < 25; index++) {
+			//var index = {index : []};		
+			ratesBuilder.push([]);
+	}
+
+	keys.forEach(key => {
+		var index = 0;
+		var next = false;
+		ratesBuilder.forEach(rateBuild => {
+			if(next) {
+				return;
+			}
+
+			var len = rateBuild.join("").length;
+			if(len < 500){
+				ratesBuilder[index].push(`${key}: ${rates[key]}`)
+				next = true;
+				return;
+			} else {
+				index += 1;
+			}
+		});
+	});
+
+	
+
+	var page = 1;
+	ratesBuilder.forEach(rateBuild => {
+		if(rateBuild.length <= 0) return;
+		var everyOther = false;
+		const longest = rateBuild.reduce((long, str) => Math.max(long, str.length), 0);
+		
+		embedRates.addField(`Page number: ${page}`, `\`\`\`${rateBuild.map(key => {everyOther = !everyOther; return `${key}${everyOther ? " ".repeat(longest - key.length + 2) : "\n"}`;}).join("")}\`\`\``);
+
+		page += 1;
+	});
+
+	message.author.send(embedRates)
+	.then(msg => {
+		const embed2 = new Discord.RichEmbed()
+			.setColor(0x78FF00)
+			.setDescription(`A list of all the rates has been compiled, and PMed to: <@${message.author.id}>. (${message.author.tag})`)
+			.setFooter("Rates supplied by Fixer.io")
+			.setTimestamp();
+	
+		if(msg){
+			msg.edit(embed2);
+		} else {
+			message.channel.send(embed2);
+		}
+	})
+	.catch(err => {
+		const embed2 = new Discord.RichEmbed()
+			.setColor(0xFA3C3C)
+			.setDescription(`Unable to PM: <@${message.author.id}>. (${message.author.tag})`)
+			.setFooter("Uh oh...")
+			.setTimestamp()
+			.addField("Error: ", `\`\`\`${err}\`\`\``);
+	
+		if(msg){
+			msg.edit(embed2);
+		} else {
+			message.channel.send(embed2);
+		}
+	});
+
+	
+
+}

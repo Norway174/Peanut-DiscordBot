d929834f92fae1eff3a944fe29838e1461e90f8d
Norway174 <willyodden@hotmail.com>
2019-07-17 02:06:52 +0200

    Update to User command

--- commands/user.js
+++ commands/user.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
 
+var _format = "MMM Do - YYYY, HH:mm";
+
 exports.run = function(client, message, args){
 	this.client = client;
 	
@@ -9,20 +11,32 @@ exports.run = function(client, message, args){
 	if (!dUser) return message.channel.send("Can't find user!")
 
 	var joinedAt = moment(dUser.joinedTimestamp);
+	var joined = joinedAt.format(_format)
 	var createdAt = moment(dUser.user.createdAt);
+	var created = createdAt.format(_format)
+
+	var lastMsg = dUser.lastMessage ? dUser.lastMessage : null;
+	function lastMesg(){
+		var timestamp = moment(dUser.lastMessage.createdTimestamp)
+		return `${timestamp.format(_format)} [${timestamp.fromNow()}]\n${lastMsg.channel}\n\`\`\`${lastMsg.cleanContent}\`\`\``;
+	}
 
 	const embed = new Discord.RichEmbed()
-		.setTitle("User: " + dUser.displayName)
+		.setAuthor(dUser.user.tag, dUser.user.displayAvatarURL)
+		.setDescription(dUser)
 		.setColor(dUser.displayColor)
 		.setFooter(message.guild.name)
-		.setImage(dUser.user.avatarURL)
-		.addField("Tag", dUser.user.tag, true)
-		.addField("ID", dUser.id, true)
-		.addField("Joined at", joinedAt.format("dddd, MMMM Do YYYY, h:mm:ss a"), true)
-		.addField("Joined ago", joinedAt.fromNow(), true)
-		.addField("Created at", createdAt.format("dddd, MMMM Do YYYY, h:mm:ss a"), true)
-		.addField("Created ago", createdAt.fromNow(), true)
+		.setThumbnail(dUser.user.avatarURL)
+		.addField("Display Name", dUser.displayName, true)
+		.addField("User ID", dUser.id, true)
 		.addField("Status", dUser.user.presence.status, true)
+		.addField("Joined server on", `${joined} [${joinedAt.fromNow()}]`)
+		.addField("Accounted Created", `${created} [${createdAt.fromNow()}]`);
+
+		lastMsg ? embed.addField("Last Message", lastMesg()) : null
+
+		embed
+		.addField(`Roles [${dUser.roles.size - 1}]`, dUser.roles.map(role => role.name === "@everyone" ? null : role).join(" "))
 		.addField("Avatar URL", dUser.user.displayAvatarURL, true)
 		.setTimestamp();
 

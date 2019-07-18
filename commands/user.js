b91f1c0083b964b1a280b856735233a154ea0907
Norway174 <willyodden@hotmail.com>
2019-07-18 03:32:12 +0200

    Improvements to the user command

--- commands/user.js
+++ commands/user.js
@@ -4,11 +4,30 @@ require("moment-duration-format");
 
 var _format = "MMM Do - YYYY, HH:mm";
 
-exports.run = function(client, message, args){
+exports.run = async function(client, message, args){
 	this.client = client;
+
+	// If a guild has more than 250 members, not every member will be cached.
+	// This should get all the members, so we can search for the correct one. Might take a while?
+	if(message.guild.memberCount >= 250) await message.guild.fetchMembers();
 	
-	let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.find(mems => mems.displayName === args.join(' '));
-	if (!dUser) return message.channel.send("Can't find user!")
+	let dUser = message.guild.member(message.mentions.users.first()) // First we try to get the first mention. Simple & Quick!
+	|| message.guild.members.get(args[0]) // Then we check if the supplied arg is a an ID.
+	|| message.guild.members.find(mems => { // If that fails, we move on to the heavy stuff.
+		if (mems.displayName.toLowerCase() === args.join(' ').toLowerCase()){ // Check if a user has the display name. Inclduing a custon nickname.
+			return mems;
+		} else
+		if (mems.user.username.toLowerCase() === args.join(' ').toLowerCase()){ // Then check their real username.
+			return mems;
+		} else
+		if (mems.user.tag.toLowerCase() === args.join(' ').toLowerCase()){ // Then we check their real username + tag.
+			return mems;
+		}
+	});
+	if (!dUser) {
+		// If all of them fails. Then we report back. :(
+		return message.channel.send("Can't find user!")
+	}
 
 	var joinedAt = moment(dUser.joinedTimestamp);
 	var joined = joinedAt.format(_format)
@@ -21,6 +40,8 @@ exports.run = function(client, message, args){
 		return `${timestamp.format(_format)} [${timestamp.fromNow()}]\n${lastMsg.channel}\n\`\`\`${lastMsg.cleanContent}\`\`\``;
 	}
 
+	var roles = dUser.roles.filter(role => { if(role.name !== "@everyone") return role } )
+
 	const embed = new Discord.RichEmbed()
 		.setAuthor(dUser.user.tag, dUser.user.displayAvatarURL)
 		.setDescription(dUser)
@@ -31,12 +52,12 @@ exports.run = function(client, message, args){
 		.addField("User ID", dUser.id, true)
 		.addField("Status", dUser.user.presence.status, true)
 		.addField("Joined server on", `${joined} [${joinedAt.fromNow()}]`)
-		.addField("Accounted Created", `${created} [${createdAt.fromNow()}]`);
+		.addField("Account Created", `${created} [${createdAt.fromNow()}]`);
 
 		lastMsg ? embed.addField("Last Message", lastMesg()) : null
 
 		embed
-		.addField(`Roles [${dUser.roles.size - 1}]`, dUser.roles.map(role => role.name === "@everyone" ? null : role).join(" "))
+		.addField(`Roles [${roles.size}]`, roles.size != 0 ? roles.array().join(" ") : 'No roles.')
 		.addField("Avatar URL", dUser.user.displayAvatarURL, true)
 		.setTimestamp();
 

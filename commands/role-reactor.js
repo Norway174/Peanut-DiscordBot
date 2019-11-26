6388b1b06c6815aa9ffc712cd6355369264bdcb5
Norway174 <willyodden@hotmail.com>
2019-11-26 04:58:36 +0100

    Added overwrite to Role-Reactor
    Let's you target another message to apply the Role-reactions to in the same channel.
    
    Simply do "#role-reactor -<message ID>" to target another message.
    
    There's no feedback if it was successfull, yet.

--- commands/role-reactor.js
+++ commands/role-reactor.js
@@ -6,9 +6,30 @@ async function asyncForEach(array, callback) {
   }
 
 exports.run = async (client, message, args) => {
+
+	const overwrite_msg_regex = /^\s*(-[0-9]{18})/;
+	
+	var overwrite_msg_id = args[0].match(overwrite_msg_regex);
+
+	//console.log(overwrite_msg_id);
+	if (overwrite_msg_id !== null ){
+		overwrite_msg_id = overwrite_msg_id[0].replace("-", "");
+		var ovr_msg = await message.channel.fetchMessage(overwrite_msg_id)
+		.catch(err => {
+			message.channel.send("Unable to find message.");
+			//client.logger.error(err);
+			return null;
+		});
+		if ( ovr_msg == null ) return;
+
+		message = ovr_msg;
+
+		client.reactionsRole.delete(overwrite_msg_id);
+	}
+
 	var msg = message.content;
 	var lines = msg.split("\n");
-	
+
 	// REGEX to find Unicode Emoji OR Discord Custom Emoji
 	const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c\ude32-\ude3a]|[\ud83c\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])|<:[a-zA-Z0-9]*:[0-9]*>/gm;
 
